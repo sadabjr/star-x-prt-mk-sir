813966fea6285189aa4ec5d18a095d3d5f3d0f15
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2024-01-22 10:39:12 +0530

    new chnage

--- src/pages/PdfViewer/PdfViewer.jsx
+++ src/pages/PdfViewer/PdfViewer.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Document, Page } from "react-pdf";
 import { useParams } from "react-router-dom";
 import { getDemoNoteById } from "../../Firebase";
+import pdf from '../../asssets/pdf/MS Paint क्या है.pdf'
 
 const PdfViewer = () => {
   const [numPages, setNumPages] = useState();
@@ -9,27 +10,26 @@ const PdfViewer = () => {
   const [pdfPath, setPdfPath] = useState(null); // Rename the state variable
   const { pdfId } = useParams();
 
-  useEffect(() => {
-    const fetchPdfPath = async () => {
-      try {
-        const demoNote = await getDemoNoteById(pdfId);
-        console.log("Fetched demoNote:", demoNote);
-    
-        if (demoNote && demoNote.pdfPath) {
-          const pdfPath = demoNote.pdfPath;
-          console.log("Resolved pdfPath:", pdfPath);
-          setPdfPath(pdfPath);
-        } else {
-          console.error("PDF path not found in demoNote:", demoNote);
-        }
-      } catch (error) {
-        console.error("Error fetching PDF path:", error);
-      }
-    };
-    
-    fetchPdfPath(); // Call the fetchPdfPath function when the component mounts or pdfId changes
-  }, [pdfId]);
-  
+  // useEffect(() => {
+  //   const fetchPdfPath = async () => {
+  //     try {
+  //       const demoNote = await getDemoNoteById(pdfId);
+  //       console.log("Fetched demoNote:", demoNote);
+
+  //       if (demoNote && demoNote.pdfPath) {
+  //         const pdfPath = demoNote.pdfPath;
+  //         console.log("Resolved pdfPath:", pdfPath);
+  //         setPdfPath(pdfPath);
+  //       } else {
+  //         console.error("PDF path not found in demoNote:", demoNote);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching PDF path:", error);
+  //     }
+  //   };
+
+  //   fetchPdfPath(); // Call the fetchPdfPath function when the component mounts or pdfId changes
+  // }, [pdfId]);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
@@ -41,20 +41,21 @@ const PdfViewer = () => {
         <p>
           Page {pageNumber} of {numPages}
         </p>
-        {pdfPath && (
-          <Document file={pdfPath} onLoadSuccess={onDocumentLoadSuccess}>
+       
+          <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
             {Array.apply(null, Array(numPages))
               .map((x, i) => i + 1)
-              .map((page) => (
-                <Page
-                  key={page}
+              .map((page) => {
+                return(
+                  <Page
                   pageNumber={page}
                   renderTextLayer={false}
                   renderAnnotationLayer={false}
                 />
-              ))}
+                )
+              })}
           </Document>
-        )}
+    
       </div>
     </div>
   );
